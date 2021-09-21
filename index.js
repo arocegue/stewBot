346ea744184ad393cb0040e82e6a3e2473b0deaf
arocegue <arocegue@ucsc.edu>
2021-09-20 20:54:02 -0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
+//const config = require("./config.json");
 const ytdl = require("ytdl-core");
 const ytSearch = require("yt-search");
 const { validateURL } = require("ytdl-core");
